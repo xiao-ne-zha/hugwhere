6dc362e0cb84e94e23e3fed4f5f1cc703519ecfa
xiaonezha <zszhang@kctimes.cn>
2023-11-06 08:44:53 +0800

    github不支持project.clj导致ci失败

--- project.clj
+++ project.clj
@@ -1,1 +1,14 @@
-project.clj.tovictory
+(defproject org.clojars.xiao-ne-zha/hugwhere "1.0.0"
+  :description "dynamic `where conditions` when you use hugsql to create your function that access database
+当你使用hugsql来编写数据库访问函数时，该库可以让你省去编写动态拼接where条件的繁琐"
+  :url "https://github.com/xiao-ne-zha/hugwhere"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.layerware/hugsql "0.5.3"]
+                 [instaparse "1.4.12"]
+                 [org.clojure/core.cache "1.0.225"]
+                 [robert/hooke "1.3.0"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :repl-options {:init-ns org.tovitory.db.hugwhere})
