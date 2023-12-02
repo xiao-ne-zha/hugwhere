6ee88a460bd60e2a8ca415173063c86962c8bba5
张志胜 <zhangzhisheng@tydic.com>
2023-12-02 20:15:58 +0800

    简化查询sql文件的实现

--- src/clj/org/tovictory/db/listsql.clj
+++ src/clj/org/tovictory/db/listsql.clj
@@ -1,12 +1,8 @@
 (ns org.tovictory.db.listsql
   (:require  [clojure.string :as str]
-             [instaparse.core :refer [defparser] :as insta]
              [clojure.core.cache :as cache]
              [clojure.java.io :as io]
-             [clojure.tools.logging :as log]
-             [hugsql.parameters :as hp]))
-
-(defparser ^:private parser (slurp (io/resource "listsql.bnf")))
+             [clojure.tools.logging :as log]))
 
 (defn join-line [& s] (str/join "\n" s))
 
@@ -25,22 +21,32 @@
    :header make-header
    :sqls make-sqls})
 
+(defn xf-line [line]
+  (let [line (str/triml line)]
+    (cond
+      (re-matches #"--\s+:list\s+\S+\s*" line)
+      (let [sqlid (re-find #"(?<=--\s+:list\s+)\S+")]
+        (str "-- :name " sqlid " :? :* :D"))
+      (re-matches #"--\s+:count\s+\S+\s*" line)
+      (let [sqlid (re-find #"(?<=--\s+:count\s+)\S+")]
+        (str "-- :name " sqlid " :? :1 :D"))
+      (str/starts-with? line "{")
+      (str "--~ " line)
+      :else
+      line)))
+
 (defn listsql->hugsql [ls]
-  (let [result (parser ls)]
-    (if (insta/failure? result)
-      (log/error "parse listsql error, listsql is " ls "\nfailure info:\n" result)
-      (insta/transform trfm-map result))))
+  (->> ls
+       str/split-lines
+       (map xf-line)
+       (str/join \newline)))
 
 (comment
-  (parser "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1 and {a = :a}}")
-  (parser "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1\n and {a = :a}}")
-  (parser "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1\n--comment\n and {a = :a}}")
-  (parser "-- :name test-expr\n--comment\n(let [s1 \"select * from table-a\"] (str s1 (when (:name params) \" where ddd\")))")
-  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1 and {a = :a}}")
-  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1\n and {a = :a}}")
-  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a {where 1 = 1\n--comment\n and {a = :a}}")
+  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a\n {where 1 = 1 and {a = :a}}")
+  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a \n{where 1 = 1\n and {a = :a}}")
+  (listsql->hugsql "-- :name list-by-map \n-- comment content\nselect * from table-a \n{where 1 = 1\n--comment\n and {a = :a}}")
   ;; 复杂的where条件，可以写成clojure语言来组织字符串
   ;; 考虑到复杂性需要对齐等因素，建议采用/*~ clojure form ~*/ 形式
-  (listsql->hugsql "-- :name test-expr\n--comment\n(let [s1 \"select * from table-a\"] (str s1 (when (:name params) \" where ddd\")))")
+  (listsql->hugsql "-- :name test-expr\n--comment\n--~(let [s1 \"select * from table-a\"] (str s1 (when (:name params) \" where ddd\")))")
   (listsql->hugsql "-- :name test-expr\n--comment\n/*~(when (:name params) \"and name = :name\")~*/")
-  (listsql->hugsql "-- :name test-expr\n--comment\n/*~\n(when (:name params) \"and name = :name\")\n~*/"))
+  (listsql->hugsql "-- :name test-expr\n--comment\n/*~\n--~(when (:name params) \"and name = :name\")\n~*/"))
